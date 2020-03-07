ca2d751c72b145513f14faee0406382773efb966
liaolp413 <965499695@qq.com>
2020-03-07 15:13:58 +0800

    创建了测试类A,由组员2首次提交

---
+++ src/main/java/cn/com/java/test/TestA.java
@@ -0,0 +1,12 @@
+package cn.com.java.test;
+
+/**
+ * @author llpei
+ * @Description:
+ * @date Created in 15:10 2020/3/7
+ */
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("创建了TestA.java,由组员2首次提交");
+    }
+}
